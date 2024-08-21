24ae405114efb808efd90bbb8d8f06a5930102a8
Michal Ziółkowski <mi.ziolkowski1@gmail.com>
2024-08-21 23:55:54 +0200

    Added tables

---
+++ WarehouseDB/Tables/Prices.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Prices]
+(
+	[Id] INT NOT NULL , 
+	[ArticleID] INT NULL, 
+	[Price] DECIMAL NULL, 
+	[Date] DATE NULL, 
+	PRIMARY KEY ([Id]), 
+	CONSTRAINT [FK_Prices_Articles] FOREIGN KEY ([ArticleID]) REFERENCES [Articles]([Id])
+)
