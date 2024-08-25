ddcea200faeea61cf18d121c90ce0ab828f6d64d
Michal Ziółkowski <mi.ziolkowski1@gmail.com>
2024-08-25 20:25:55 +0200

    Added mssql db support

---
+++ AS-MSSQL-API/Models/ArticleModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AS_MSSQL_API.Models
+{
+	public class ArticleModel
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string? Description { get; set; }
+	}
+}
