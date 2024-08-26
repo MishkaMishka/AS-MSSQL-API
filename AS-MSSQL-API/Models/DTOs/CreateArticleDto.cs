ab99d6f18b90d5764d63f768505968c9f4dcc9b5
Michal Ziółkowski <mi.ziolkowski1@gmail.com>
2024-08-26 15:57:29 +0200

    Added DTOs

---
+++ AS-MSSQL-API/Models/DTOs/CreateArticleDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AS_MSSQL_API.Models.DTOs
+{
+	public class CreateArticleDto
+	{
+		[Required]
+		[MaxLength(100)]
+		public string Name { get; set; }
+
+		[MaxLength(500)]
+		public string Description { get; set; }
+
+		public List<CreatePriceDto> Prices { get; set; }
+	}
+}
---
+++ AS-MSSQL-API/Models/DTOs/CreatePriceDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AS_MSSQL_API.Models.DTOs
+{
+	public class CreatePriceDto
+	{
+		[Required]
+		public decimal Price { get; set; }
+
+		[Required]
+		public DateTime Date { get; set; }
+	}
+}
