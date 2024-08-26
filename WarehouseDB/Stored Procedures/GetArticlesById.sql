d52fa9616bdf2f95ee56db610e27e96fe8a04efb
Michal Ziółkowski <mi.ziolkowski1@gmail.com>
2024-08-26 15:57:06 +0200

    Fixed procedure for single record get

---
+++ WarehouseDB/Stored Procedures/GetArticlesById.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetArticleById]
+	@ArticleId INT
+AS
+BEGIN
+	SELECT 
+		a.Id,
+		a.Name,
+		a.Description,
+		(SELECT Price, Date FROM Prices WHERE ArticleId = a.Id FOR JSON PATH) AS Prices
+	FROM Articles a
+	WHERE a.Id = @ArticleId
+	FOR JSON PATH, WITHOUT_ARRAY_WRAPPER;
+END
+GO
