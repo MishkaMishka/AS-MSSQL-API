4288393d603e63bf5e77fd6f44bba3b310a8788e
Michal Ziółkowski <mi.ziolkowski1@gmail.com>
2024-08-26 16:01:04 +0200

    Added jsonSerializer option to handle issue with possible object cycle

--- AS-MSSQL-API/ApplicationDbContext.cs
+++ AS-MSSQL-API/ApplicationDbContext.cs
@@ -3,16 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
-
-    public DbSet<ArticleModel> Articles { get; set; }
-    public DbSet<PriceModel> Prices { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        // Opcjonalna konfiguracja modelu
-    }
+	public DbSet<ArticleModel> Articles { get; set; }
+	public DbSet<PriceModel> Prices { get; set; }
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 }
--- AS-MSSQL-API/Program.cs
+++ AS-MSSQL-API/Program.cs
@@ -9,15 +9,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllers()
+	.AddJsonOptions(options =>
+	{
+		options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+	});
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
