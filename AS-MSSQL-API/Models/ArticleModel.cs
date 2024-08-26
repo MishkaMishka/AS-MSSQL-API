65e8e373714b6c43ef7a8563a5b155ed8525e6d9
Michal Ziółkowski <mi.ziolkowski1@gmail.com>
2024-08-26 15:58:34 +0200

    Updated models & added data annotations

--- AS-MSSQL-API/Models/ArticleModel.cs
+++ AS-MSSQL-API/Models/ArticleModel.cs
@@ -1,11 +1,20 @@
-﻿namespace AS_MSSQL_API.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AS_MSSQL_API.Models
 {
-	public class ArticleModel
-	{
-		public int Id { get; set; }
+    public class ArticleModel
+    {
+        [Key]
+        public int Id { get; set; }
 
-		public string Name { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
 
-		public string? Description { get; set; }
-	}
+        [MaxLength(500)]
+        public string Description { get; set; }
+        public ICollection<PriceModel> Prices { get; set; }
+    }
+
 }
--- AS-MSSQL-API/Models/PriceModel.cs
+++ AS-MSSQL-API/Models/PriceModel.cs
@@ -1,15 +1,25 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AS_MSSQL_API.Models
 {
 	public class PriceModel
 	{
+        [Key]
         public int Id { get; set; }
 
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        // Klucz obcy - relacja do artykułu
         public int ArticleId { get; set; }
 
-        public float Price { get; set; }
-
-        public DateTime Date { get; set; }
+        // Nawigacja w drugą stronę
+        [ForeignKey("ArticleId")]
+        public ArticleModel Article { get; set; }
     }
 }
